df5de299a7744275e1c1431b2e0b2ac72ac718f5
Kate <ekaterina21051988@yandex.by>
2024-01-03 19:28:03 +0200

    fix: user service has been fixed

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,7 @@
 const API_PATHS = {
   product: "https://hjfant4yl8.execute-api.eu-west-1.amazonaws.com",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/prod",
+  order:
+    "http://katsiarynamizhuryna-cart-api-dev.eu-west-1.elasticbeanstalk.com/",
   import: "https://ocyeq7jh0j.execute-api.eu-west-1.amazonaws.com",
   bff: "https://hjfant4yl8.execute-api.eu-west-1.amazonaws.com",
   cart: "http://katsiarynamizhuryna-cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
